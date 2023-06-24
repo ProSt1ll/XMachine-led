39357b0d819f9cb5dd90d71ad42cf08a3a6803f9
alex yashnyk <alex.yashnyk@gmail.com>
2023-06-24 21:47:51 +0300

    Update XMachine-led.ino
    тесты к съемкам

--- XMachine-led.ino
+++ XMachine-led.ino
@@ -7,8 +7,8 @@
 #define LED_TYPE NEOPIXEL
 #define NUM_STRIPS 8       
 //#define LED_TYPE   WS2815
-#define MAX_BRIGHT 220
-#define MIN_BRIGHT 10
+#define MAX_BRIGHT 240
+#define MIN_BRIGHT 40
 #define BRIGHT_1 30
 #define BRIGHT_2 100
 #define BRIGHT_3 150
@@ -19,8 +19,8 @@
 #define FIRST NUM_LEDS_METR*1
 #define SECOND 2*NUM_LEDS_METR
 #define THIRD 3*NUM_LEDS_METR
-#define FOURTH 4.5*NUM_LEDS_METR
-#define FIFTH 5.5*NUM_LEDS_METR+10
+#define FOURTH 4.5*NUM_LEDS_METR+40
+#define FIFTH 5.5*NUM_LEDS_METR-19
 #define SIXTH 6*NUM_LEDS_METR+16
 #define SEVENTH 144
 #define EIGHTH 144
@@ -28,19 +28,19 @@
 
 #define STEPS_CNT_RANGE 7
 #define MAX_RANGE 1024
-#define MIN_RANGE 80
+#define MIN_RANGE 146
 
 double stepp= MAX_RANGE/STEPS_CNT_RANGE;
-
-
+bool alarm_flag=false;
+int type_processing=0;
 //byte COLOR = 0; 
 byte BRIGHTNESS = 0;
 byte STEP_BRIGHT;
 uint8_t current_color;
 int j_pred=STEPS_CNT_RANGE;
-int val_pred;
-int val_predd=MAX_RANGE;
-#define OFFSET 8
+double val_pred;
+double val_predd=MAX_RANGE;
+#define OFFSET 9
 int num_leds[8] ={FIRST,SECOND,THIRD,FOURTH,FIFTH,SIXTH,SEVENTH,EIGHTH};
 
 
@@ -61,7 +61,7 @@ void setup() {
   pinMode(0,INPUT_PULLUP);
   pinMode(A0,INPUT_PULLUP);
   
-   Serial.begin(115200);
+   Serial.begin(9600);
 
   //A0, A1, A2 as outputs.
 //  leds[0]=leds1;
@@ -71,14 +71,6 @@ void setup() {
 //  leds[4]=leds5;
 //  leds[5]=leds6;
 //  leds[6]=leds7;
-  leds[0]=leds1;
-  leds[1]=&leds2[0];
-  leds[2]=leds2;
-  leds[3]=leds4;
-  leds[4]=leds5;
-  leds[5]=leds6;
-  leds[6]=leds7;
-  leds[7]=leds8;
 
 
   FastLED.addLeds<LED_TYPE, 8>(leds1, FIRST,0);
@@ -89,18 +81,26 @@ void setup() {
   FastLED.addLeds<LED_TYPE, 3>(leds7, SEVENTH);
   FastLED.addLeds<LED_TYPE, 5>(leds8, EIGHTH);
 
+  leds[0]=leds1;
+  leds[1]=&leds2[0];
+  leds[2]=leds2;
+  leds[3]=leds4;
+  leds[4]=leds5;
+  leds[5]=leds6;
+  leds[6]=leds7;
+  leds[7]=leds8;
   
-  COLOR=128;
+  COLOR=0;
   SATURATION2=0;
   
-  BRIGHT[0]=100;
-  BRIGHT[1]=100;
-  BRIGHT[2]=100; 
-  BRIGHT[3]=100;
-  BRIGHT[4]=100;
-  BRIGHT[5]=100;
-  BRIGHT[6]=60;
-  BRIGHT[7]=180;  
+  BRIGHT[0]=255;
+  BRIGHT[1]=255;
+  BRIGHT[2]=255; 
+  BRIGHT[3]=255;
+  BRIGHT[4]=255;
+  BRIGHT[5]=255;
+  BRIGHT[6]=255;
+  BRIGHT[7]=255;  
 //
 //         for(int i=0;i<NUM_STRIPS;i++){
 //    //turn_on(i,COLOR,SATURATION,BRIGHTNESS)
@@ -115,15 +115,35 @@ void setup() {
 //    turn_on(i,0,0,MIN_BRIGHT);
 //    }
 //  }
-  processing(0);
-  delay(10000);
+//   FastLED.showColor(CRGB::White);
+// turn_on(0,COLOR,SATURATION2,BRIGHT[0]);
+ processing(0);
+  j_pred=-2;
+ val_predd=-100;
+  delay(3000);
 }
-  void loop(){
-    read_ports();
-  }
-void initLED(){
 
-    FastLED.showColor(CRGB::White);
+
+  void loop(){
+  
+ //   test();
+    read_ports();
+    //checkChange();
+  }
+
+void test(){
+  for (int i=126;i<860;){
+
+    processing2(i);
+    i=i+20;
+
+    }
+    for (int i=860;i>126;){
+    processing2(i);
+    i=i-20;
+
+    }
+   
   }
 void read_buttons() {
   bool CHECK_BTN_2_STATE = digitalRead(11);
@@ -150,40 +170,101 @@ void read_buttons() {
  
 } 
 
-bool alarm_flag=false;
 void read_ports() {
   int val = -1;
   val=analogRead(A0);
     Serial.println("val:");   
   Serial.println(val);
    Serial.println(MAX_RANGE-val);
-  //int flag = digitalRead(0);
-  processing(MAX_RANGE-val);
-//  if(flag == LOW){
-//    Serial.println("process");
-//    processing(val);
-//    }
+ Serial.print("type_processing: ");
+   Serial.println(type_processing);
+   processing2(MAX_RANGE-val);   
+// switch(type_processing){
+//  case 0:
+//     processing2(MAX_RANGE-val);
+//   
+//    break;
+//  case 1:
+//   breathe();
+//    break;
+//  case 2:
+//    processing(MAX_RANGE-val);   
+//    break;
+//  }
+
 }
 
-void checkAl(int val){
-  bool change=false;
+void breathe(){
+
+    for (int bright=MIN_BRIGHT;bright<MAX_BRIGHT;){
+    for(int i=0;i<NUM_STRIPS;i++){
+    //turn_on(i,COLOR,SATURATION,BRIGHTNESS)
+    // i - не трогать, остальные значения 0-255
+    turn_on(i,100,0,bright);
+    
+    }
+    bright=bright+10;
+    FastLED.show();
+    FastLED.show();
+    }
+    
+    for (int bright=MAX_BRIGHT;bright>MIN_BRIGHT;bright--){
+    for(int i=0;i<NUM_STRIPS;i++){
+    //turn_on(i,COLOR,SATURATION,BRIGHTNESS)
+    // i - не трогать, остальные значения 0-255
+    turn_on(i,100,0,bright);
+    }
+    bright=bright-10;
+      FastLED.show();
+    FastLED.show();
+    }
+  
+  }
+void checkChange(){
+  int val =digitalRead(0);
+
   delay(50);
   if (val!=digitalRead(0)){
     return;
     }
-  Serial.println(val);  
+
+  if(val !=0){
+    return;
+    }  
+  type_processing++;
+  if (type_processing>3){
+    type_processing=0;
+    }
+    j_pred=STEPS_CNT_RANGE;
+
+ val_predd=MAX_RANGE;
+ processing(0);
+  }
+
+  
+void checkAl(){
+  int val=digitalRead(0);
+  delay(50);
+  if (val!=digitalRead(0)){
+    return;
+    }
+
+   Serial.print("val");
+   Serial.println(val);
   if (val==0){
     if(!alarm_flag){
       Serial.println("alarm");
+ Serial.println("alarm");
       alarm();
+     
       }
   alarm_flag=true;   
   }else{
     if(alarm_flag){
       Serial.println("norm");
-      whiteC();
-      }
     alarm_flag=false;
+    whiteC();
+      }
     }
     
 }
@@ -304,6 +385,202 @@ FastLED.show();
       
 }
 
+
+
+void processing2(int val){
+    if(abs(val-val_predd)<OFFSET){
+      return;
+      }
+    
+    if (val>MAX_RANGE){
+      val=MAX_RANGE;
+    }
+    if (val<MIN_RANGE){
+      val = MIN_RANGE;
+      }
+    
+     val_predd = val;
+
+       
+    Serial.print("step:");
+    Serial.println(val/stepp);
+    int j;
+    double temp;
+    double K;
+    j=val/stepp;
+    K=255/stepp;
+    Serial.print("j:");
+    Serial.println(j);
+    Serial.print("j_pred:");
+    Serial.println(j_pred);
+    if(j !=j_pred){
+      if(j-j_pred>0){
+          //smooth(j_pred,val_pred,MAX_BRIGHT);
+          for(int i=j_pred;i<=j-2;i++){
+          Serial.print("off:");
+          Serial.println(i);
+          turn_on2(i,MIN_BRIGHT);
+          }  
+           turn_on2(j,MIN_BRIGHT);
+          val_pred=MIN_BRIGHT;
+          
+        }else{
+            for(int i=j_pred;i>j+2;i--){
+            Serial.print("off:");
+            Serial.println(i);
+            turn_on2(i,MIN_BRIGHT);
+          }
+          turn_on2(j-3,MIN_BRIGHT);
+          val_pred=MAX_BRIGHT;
+        }
+
+    j_pred=j;
+    return;
+    }
+    
+    temp=double(val%int(stepp));
+    temp=K*temp;
+
+    double off_val=255-temp;
+    if(temp<MIN_BRIGHT){
+      temp=MIN_BRIGHT;
+      }
+    if(temp>MAX_BRIGHT){
+      temp=MAX_BRIGHT;
+      }  
+
+    if(off_val<MIN_BRIGHT){
+      off_val=MIN_BRIGHT;
+      }
+    if(off_val>MAX_BRIGHT){
+      off_val=MAX_BRIGHT;
+      }  
+
+     if(abs(temp-val_pred)<OFFSET){
+      return;
+      }
+      Serial.print("val_pred:");
+      Serial.println(val_pred);
+    Serial.print("on led:");
+    Serial.println(temp);
+    Serial.print("off led:");
+    Serial.println(off_val);
+    if(temp==MIN_BRIGHT){
+      return;
+      }
+    turn_on2(j,temp);
+    turn_on2(j-2,off_val);
+    
+    val_pred=temp;
+    
+  }
+
+void processing3(int val,bool clear_leds){
+    if(abs(val-val_predd)<OFFSET){
+      return;
+      }
+    
+    if (val>MAX_RANGE){
+      val=MAX_RANGE;
+    }
+    if (val<MIN_RANGE){
+      val = MIN_RANGE;
+      }
+    
+     val_predd = val;
+
+       
+    Serial.print("step:");
+    Serial.println(val/stepp);
+    int j;
+    double temp;
+    double K;
+    j=val/stepp;
+    K=255/stepp;
+    Serial.print("j:");
+    Serial.println(j);
+    Serial.print("j_pred:");
+    Serial.println(j_pred);
+    if(j !=j_pred){
+      if(j-j_pred>0){
+          //smooth(j_pred,val_pred,MAX_BRIGHT);
+          //turn_on2(j,MIN_BRIGHT);
+          val_pred=MIN_BRIGHT;
+          
+        }else{
+          turn_on2(j-3,MIN_BRIGHT);
+          val_pred=MAX_BRIGHT;
+        }
+
+    
+
+    j_pred=j;
+    return;
+    }
+    
+    temp=double(val%int(stepp));
+    temp=K*temp;
+
+    double off_val=255-temp;
+    if(temp<MIN_BRIGHT){
+      temp=MIN_BRIGHT;
+      }
+    if(temp>MAX_BRIGHT){
+      temp=MAX_BRIGHT;
+      }  
+
+    if(off_val<MIN_BRIGHT){
+      off_val=MIN_BRIGHT;
+      }
+    if(off_val>MAX_BRIGHT){
+      off_val=MAX_BRIGHT;
+      }  
+
+     if(abs(temp-val_pred)<OFFSET){
+      return;
+      }
+      Serial.print("val_pred:");
+      Serial.println(val_pred);
+    Serial.print("on led:");
+    Serial.println(temp);
+    Serial.print("off led:");
+    Serial.println(off_val);
+    if(temp==MIN_BRIGHT){
+      return;
+      }
+  if (clear_leds){
+   for(int i=0;i<6;i++){
+    turn_on(i,0,0,MIN_BRIGHT);
+    
+    }
+  }
+    turn_on2(j,temp);
+    turn_on2(j-2,off_val);
+    val_pred=temp;
+    
+  }
+ void clear_strip(int strip){
+     int nums;
+    int i;
+     if(strip == 1){
+      i=THIRD;
+      nums=SECOND+THIRD;
+      }else if(strip==2){
+       i=0;
+       strip=1;
+       nums=THIRD-1; 
+      }else{
+        i=0;
+        nums=num_leds[strip];
+      }
+     for(; i <nums; i++) {
+      leds[strip][i] = CHSV(0,0,0);
+      }
+      //FastLED.show();
+  
+  }
+
+
  void turn_on(int strip,int color,int saturation,int brightness){
     int nums;
     int i;
@@ -346,6 +623,9 @@ FastLED.show();
   }
 
 void turn_on2(int strip,int color){
+    if (strip==0){
+      return;
+      }
     int nums;
     int i;
      if(strip == 1){
@@ -354,7 +634,7 @@ void turn_on2(int strip,int color){
       }else if(strip==2){
        i=0;
        strip=1;
-       nums=THIRD; 
+       nums=THIRD-1; 
       }else{
         i=0;
         nums=num_leds[strip];
@@ -419,7 +699,7 @@ void turn_on2(int strip,int color){
       }
   
           
-        }
+      }
 //     
 //     if(strip == 1){
 //      i=THIRD;
